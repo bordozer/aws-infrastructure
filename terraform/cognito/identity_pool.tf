d9a271c8106b2cee5df0b3b5582a5c50cefeeefd
Boris Lukianov <boris.lukianov@seniordev.com>
2020-08-06 15:19:14 +0300

    TF: 6

--- terraform/cognito/identity_pool.tf
+++ terraform/cognito/identity_pool.tf
@@ -1,10 +1,9 @@
 resource "aws_cognito_identity_pool" "main" {
-  identity_pool_name               = "identitypool"
+  identity_pool_name               = replace("${local.aws_service_name}_identity_pool", "-", "_")
   allow_unauthenticated_identities = false
 
   cognito_identity_providers {
     client_id               = aws_cognito_user_pool_client.client.id
-//    provider_name           = aws_cognito_user_pool.pool.id
     provider_name           = "cognito-idp.${var.region}.amazonaws.com/${aws_cognito_user_pool.pool.id}"
     server_side_token_check = true
   }
